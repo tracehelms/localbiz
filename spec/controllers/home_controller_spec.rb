94e7bbfa57e5da0d243cf813f428a877130ccfcc
Trace Helms <tracehelms@gmail.com>
2013-10-12 16:06:34 -0400

    Remove test/ folder and add preliminary tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe HomeController do
 
+  it 'should get index' do
+    get :index
+    expect(response).to be_success
+    expect(response.status).to eq(200)
+  end
+
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe UserController do
 
+  describe 'when not logged in' do
+    it 'should not get index' do
+      pending
+      # get :index
+      # expect(response).to be_redirect
+      # expect(response.status).to eq(200)
+    end
+  end
+
 end
