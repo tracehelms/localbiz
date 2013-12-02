7dad5c06bd58fffcf9c3d5eb062eb88d1764e03d
Trace Helms <tracehelms@gmail.com>
2013-12-01 19:23:56 -0500

    Get Google Maps working on home page

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
 
   validates_presence_of :city, :state, :zip
+
+  def to_s
+    "#{line1} #{city}, #{state} #{zip}"
+  end
 end
